fdcdb5a5cc9457ba4fbdb384de5a7f94e7bbc85e
“gitrajkumar” <rajkumard24@gmail.com>
2023-05-23 16:08:39 +0000

    INCm83838834 - CUSTOMER IMPACTED INCIDENT - RAJA - LOANDISBURSALSESSIONBEAN.JAVA - VERSION2 - CHMR9894471330 - RELEASE DATE 23-05-23

--- LoanDisbursalsessionbean.java
+++ LoanDisbursalsessionbean.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.lang.Math;  
 import java.io.printer; 
 import java.io.collection; 
-import java.io.scanner;
 import java.io.outputprinter;
 import java.io.inputprinter;
 public class SpiralPatternExample2
